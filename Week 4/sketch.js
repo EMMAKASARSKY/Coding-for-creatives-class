a90ff3d9d373f87a8f9e4c04595090c3cfa68d6b
Emma Kasarsky <emmakasarsky@emmas-MacBook-Pro-3.local>
2017-01-31 20:42:22 -0500

    add week 4

---
+++ Week 4/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+	createCanvas (640,500);
+}
+
+function draw() {
+  	background(229, 255, 204)
+  	stroke(255, 153, 204);
+  	fill(255, 51, 153);
+  	rect(100, 200, 75, 125);
+  	stroke(51, 51, 255);
+  	fill(0, 128, 255);
+  	ellipse (mouseX, mouseY, 100, 80);
+  	stroke(255, 128, 0);
+  	line (400, 300, 150, 400);
+  }
+
+  
+
